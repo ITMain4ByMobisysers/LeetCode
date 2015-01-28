f1b174a79f503d9accb80d1d2f91985a36079388
ITMain4ByMobisysers <1084057179@qq.com>
2015-01-28 15:17:05 +0800

    求数组中个数超过n/2的元素
    一个计数器即可，当前不同的数大于前一数的计数时（count减到0），重新计数

---
+++ LeetCodeNo169--MajorityElement(By tian)/MajorityElement.java
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public int majorityElement(int[] num) {
+      int count = 0;
+      int number = 0;
+      for(int i = 0; i< num.length; i++){
+          if(count == 0){
+              count = 1;
+              number = num[i];
+          }else{
+              if(number == num[i]){
+                  count ++;
+              }else{
+                  count --;
+              }
+          }
+      }
+      return number;
+    }
+   
+}

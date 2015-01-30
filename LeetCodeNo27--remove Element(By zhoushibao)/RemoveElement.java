27109e017818e51595ec794a6d0634143f95cbae
ITMain4ByMobisysers <1084057179@qq.com>
2015-01-30 11:51:57 +0800

    第27题源码提交

---
+++ LeetCodeNo27--remove Element(By zhoushibao)/RemoveElement.java
@@ -0,0 +1,22 @@
+﻿package test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+/**
+ * Given an array and a value, remove all instances of that value in place and return the new length。
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ * @author zhoushibao
+ * 
+ *
+ */
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        int count = 0;
+		for(int i = 0; i < A.length; i ++){
+			if(A[i] != elem){
+				A[count ++] = A[i];
+			}
+		}
+		return count;
+    }
+}
